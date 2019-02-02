cfa72f4b2b660b7a64aa8e888a32e00dced85c9f
Ph0enixKM <pkaras.it@gmail.com>
2019-02-02 15:57:04 +0100

    Tabs - CSS

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import search from './search'
 import windowBar from './window_bar'
+import tabs from './tabs'
 // This is for parcel to not compile or else we get error :(
 const electron = eval(`require('electron')`)
 
@@ -7,4 +8,5 @@ const electron = eval(`require('electron')`)
 document.addEventListener('DOMContentLoaded', () => {
     search()
     windowBar(electron)
+    tabs()
 })
